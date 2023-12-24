c3e7da14539cd9d0fcbbec82fb4945167a49e3be
alexconstanza <alexconstanza@gmail.com>
2023-12-24 16:28:21 -0600

    new changes

---
+++ scripts/main.js
@@ -0,0 +1,4 @@
+const miTitulo= document.querySelector("h1");
+miTitulo.textContent="Gabriel Feo";
+const miPrueba= document.querySelector("h2");
+miPrueba.textContent= "Este es la prueba que estoy haciendo para verificar su funcionamiento."
